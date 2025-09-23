4c581e97331a2af0846d8e8b8e9f4dca07d89693
Anshu Prakash <anshuprakash55@gmail.com>
2025-09-23 17:30:27 +0530

    project complete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -9,6 +11,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Middleware to parse JSON requests
 app.use(express.json()); // allows us to parse incoming requests:req.body
@@ -16,6 +21,14 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 
 // Auth routes
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 // Start server
 app.listen(PORT, () => {
